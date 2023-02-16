f85d2cf2e86d2613759bffdadb964dcd4b53a31c
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-02-16 14:48:57 +0000

    [3]

--- db/migrate/20230212143232_create_books.rb
+++ db/migrate/20230212143232_create_books.rb
@@ -1,4 +1,5 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
+
   def change
     create_table :books do |t|
       t.string:title
@@ -6,4 +7,5 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.timestamps
     end
   end
+
 end
